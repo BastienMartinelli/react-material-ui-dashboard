e21cf3777bc7d54f311dbfb8c0c330c6793e5fe8
Bastien <bastien@pop-os.localdomain>
2019-12-10 23:40:12 +0100

    other colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,11 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { teal, pink } from "@material-ui/core/colors";
+import {
+  teal,
+  pink,
+  deepPurple,
+  grey,
+  blueGrey
+} from "@material-ui/core/colors";
 
 export const lightTheme = createMuiTheme({
   palette: {
@@ -18,13 +24,13 @@ export const lightTheme = createMuiTheme({
 export const darkTheme = createMuiTheme({
   palette: {
     primary: {
-      main: "#575A89"
+      main: blueGrey[500]
     },
     secondary: {
-      main: pink[500]
+      main: pink[400]
     },
     error: {
-      main: pink[500]
+      main: pink[400]
     },
     type: "dark"
   }
